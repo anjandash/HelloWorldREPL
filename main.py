4d35eb734e06b9b07ad97f1e83ff92db6bdf5983
anjandash1 <anjan8@gmail.com>
2021-07-08 14:32:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os,sys
+
+def max(a,b):
+  if a > b:
+    return a
+  else:
+    return b
+
+if __name__ == "__main__":
+
+  a = int(input('Enter first number: '))
+  b = int(input('Enter second number: '))
+
+  max_value = max(a,b)
+  print("The greater number is: ", max_value)
