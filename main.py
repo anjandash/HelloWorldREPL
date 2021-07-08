3fa06e6f46ef3ddf1f0bf94e38c7f79f7c326f90
anjandashx <anjan8@gmail.com>
2021-07-08 14:39:13 +0000

    Final changes!

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
   b = int(input('Enter second number: '))
 
   max_value = max(a,b)
+  print("\n****\n")
   print("The greater number is: ", max_value)
-  print("Done!")
